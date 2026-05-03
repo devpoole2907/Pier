bb820e7f4deb19ce17dcf21c96bdeabbf83a5c10
James Poole <jamesdpoole@icloud.com>
2026-05-03 13:45:10 +1200

    Initial Commit

---
+++ Pier/PierApp.swift
@@ -0,0 +1,17 @@
+//
+//  PierApp.swift
+//  Pier
+//
+//  Created by James Poole on 03/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct PierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
